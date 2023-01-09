e9fef785a1ec49bb766260da352de5dfad402428
David Veliath <60881032+PunkSamurai@users.noreply.github.com>
2023-01-09 11:00:43 +0530

    edit 160a

--- task/heapSort/160a.cpp
+++ task/heapSort/160a.cpp
@@ -33,18 +33,19 @@ int main(void)
     int n, s1=0, s2=0, i=0;
     cin >> n;
     int ar[n+1];
+    ar[0]=0;
     for(i=1; i<=n; i++)
     {
         cin >> ar[i];
         s1+=ar[i];
     }
-    heapSort(ar, n); 
-    i=1;
-    while(s2<s1)
+    heapSort(ar, n+1);
+    i=0;
+    while(s2<=s1)
     {
-        s2+=ar[i];
-        s1-=ar[i];
+        s1-=ar[n-i];
+        s2+=ar[n-i];
         i++;
     }
-    cout << n-(i-1);
+    cout << i;
 }
